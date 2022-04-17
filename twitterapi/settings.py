85cf4678e1a923c718b8667a93d6d905eed2058d
unniznd <au6695@gmail.com>
2022-04-17 09:59:28 +0530

    settings updates

--- twitterapi/settings.py
+++ twitterapi/settings.py
@@ -117,6 +117,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_DIRS=[
+    BASE_DIR/"predictor/static/"
+]
+STATIC_ROOT = BASE_DIR/"static/"
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
